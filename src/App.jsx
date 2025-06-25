fc4aba762597b313e8473187bbc2323626e09d14
luiggi00 <luiggicostarodrigues@gmail.com>
2025-06-25 13:39:05 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function BacBoApp() {
   return (
-    <div style={{ fontSize: "32px", padding: "40px", color: "white" }}>
+    <div style={{ fontSize: "32px", padding: "40px", color: "white", textAlign: "center", backgroundColor: "#111827", minHeight: "100vh" }}>
       ✅ Funciona! Agora sim o React apareceu!
     </div>
   );
